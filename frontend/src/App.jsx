5e6ed86fe21b4e38418f9b97571834f15127852b
SK ZAHIRUL ISLAM <skzahirulislam22@gmail.com>
2025-02-09 19:34:52 +0530

    Update routing paths and adjust placeholder opacity in input fields

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import Navbar from "./components/Navbar";
-
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
-
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
-
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
@@ -39,16 +36,16 @@ const App = () => {
 
       <Routes>
         <Route
-          path="/"
+          path="/chat"
           element={authUser ? <HomePage /> : <Navigate to="/login" />}
         />
         <Route
           path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          element={!authUser ? <SignUpPage /> : <Navigate to="/chat" />}
         />
         <Route
           path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          element={!authUser ? <LoginPage /> : <Navigate to="/chat" />}
         />
         <Route path="/settings" element={<SettingsPage />} />
         <Route
